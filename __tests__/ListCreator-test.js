c23046eaba7c18fbe09783d1e833e213c20dd29c
Joel <contact@joelrodriguezaleman.com>
2016-03-19 16:49:43 +0100

    Remove ListRenderer class and add tests
    I will add more classes later on, when
    I have a better understanding of how
    React works

--- __tests__/ListCreator-test.js
+++ __tests__/ListCreator-test.js
@@ -4,8 +4,24 @@ var TestUtils = require('react-addons-test-utils');
 var ListCreator = require('../src/scripts/components/ListCreator');
 
 describe('ListCreator', function() {
-    it('should add a new list', function() {
-        var listCreator = TestUtils.renderIntoDocument(<ListCreator/>);
+
+    var listCreator;
+    var event;
+
+    beforeEach(function() {
+        listCreator = TestUtils.renderIntoDocument(<ListCreator/>);
+
+        event = {
+            keyCode : 13,
+            target : {
+                disabled : true,
+                id : 'list_1',
+                value : 'Another name'
+            }
+        };
+    });
+
+    it('adds a new list', function() {
         listCreator.refs.name.value = 'My new list';
         
         var button = listCreator.refs.button;
@@ -18,5 +34,34 @@ describe('ListCreator', function() {
         setImmediate(function() {
             expect(lists.length).toEqual(1);
         });
+    });
+
+    it('enables the list to be edited', function() {
+        listCreator.toggleListEdition(event);
+
+        expect(event.target.disabled).toEqual(false);
+    });
+
+    it('updates the list', function() {
+        listCreator.state.lists.list_1 = {
+            name : 'List 1'
+        };
+
+        listCreator.updateList(event);
+
+        expect(listCreator.state.lists.list_1.name).toEqual(
+            'Another name'
+        );
+    });
+
+    it('disables the list input text after updating it', function() {
+        listCreator.state.lists.list_1 = {
+            name : 'List 1'
+        };
+        event.target.disabled = false;
+
+        listCreator.updateList(event);
+
+        expect(event.target.disabled).toEqual(true);
     });
 });
--- src/scripts/components/ListCreator.js
+++ src/scripts/components/ListCreator.js
@@ -1,5 +1,3 @@
-import ListRenderer from './ListRenderer';
-
 (function () {
    'use strict';
 
@@ -19,8 +17,28 @@ import ListRenderer from './ListRenderer';
             var timestamp = (new Date()).getTime();
             var list = {name : this.refs.name.value};
 
-            this.state.lists['list-' + timestamp] = list;
+            this.state.lists['list_' + timestamp] = list;
             this.setState({lists : this.state.lists});
+        },
+
+        toggleListEdition : function(event) {
+            event.target.disabled = !event.target.disabled;
+        },
+
+        updateList : function(event) {
+            if (event.keyCode === 13) {
+                this.state.lists[event.target.id].name =
+                    event.target.value;
+                this.toggleListEdition(event);
+            }
+        },
+
+        renderList : function(key) {
+            return (
+                <li className="list" key={key}>
+                    <input type="text" id={key} ref="list" defaultValue={this.state.lists[key].name} onClick={this.toggleListEdition} onKeyDown={this.updateList} disabled/>
+                </li>
+            )
         },
 
         render : function() {
@@ -30,7 +48,9 @@ import ListRenderer from './ListRenderer';
                         <input type="text" ref="name"/>
                         <input type="submit" ref="button" value="Create new list"/>
                     </form>
-                    <ListRenderer lists={this.state.lists} />
+                    <ul>
+                        {Object.keys(this.state.lists).map(this.renderList)}
+                    </ul>
                 </div>
             )
         }
