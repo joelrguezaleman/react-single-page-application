429b8377c86ca6fa5e909f869d37f4713a28e37c
Joel <contact@joelrodriguezaleman.com>
2016-03-20 16:44:20 +0100

    Create routes for home, list detail and 404 page

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,6 +1,12 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+var ReactRouter = require('react-router');
+var Router = ReactRouter.Router;
+var Route = ReactRouter.Route;
+var Navigation = ReactRouter.Navigation;
+var createBrowserHistory = require('history/lib/createBrowserHistory');
+
 import ListCreator from './components/ListCreator';
 
 var App = React.createClass({
@@ -14,4 +20,20 @@ var App = React.createClass({
     }
 });
 
-ReactDOM.render(<App/>, document.querySelector('#main'));
+var NotFound = React.createClass({
+    render : function() {
+        return (
+            <h1>Page not found</h1>
+        );
+    }
+});
+
+var routes = (
+    <Router history={createBrowserHistory()}>
+        <Route path="/" component={App} />
+        <Route path="/list/:list_id" component={App} />
+        <Route path="*" component={NotFound} />
+    </Router>
+);
+
+ReactDOM.render(routes, document.querySelector('#main'));
